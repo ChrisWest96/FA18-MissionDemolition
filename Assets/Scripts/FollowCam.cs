b56150c5ad345de92a86d0b4a1ba1e5743535f82
Chris West <christopherwest1996@gmail.com>
2018-10-05 15:55:56 -0400

    Commit 7
    Fixed the camera issue from Commit 6, the correct code is now:
    
    this.GetComponent<Camera>().orthographicSize = destination.y + 10;
    
    which replaced the function of:
    this.camera.orthographicSize = destination.y + 10;

--- Assets/Scripts/FollowCam.cs
+++ Assets/Scripts/FollowCam.cs
@@ -53,6 +53,6 @@ public class FollowCam : MonoBehaviour
         transform.position = destination;
         // Set the orthographicSize of the Camera to keep Ground in view
         // Set the orthographicSize of the Camera to keep Ground in view
-        this.camera.orthographicSize = destination.y + 10;
+        this.GetComponent<Camera>().orthographicSize = destination.y + 10;
     }
 }
