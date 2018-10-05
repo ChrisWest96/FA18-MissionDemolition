656c7696c00651bd881ee1710c2bfe35a011997c
Chris West <christopherwest1996@gmail.com>
2018-10-05 15:07:50 -0400

    Commit 5
    Create my own cloud prefabs, before realising there is already a script autogenerating random clouds provided. I thought the systematic generation of clouds is much more compelling than 4 or 5 prefabs.

--- Assets/Scripts/FollowCam.cs
+++ Assets/Scripts/FollowCam.cs
@@ -4,6 +4,8 @@ public class FollowCam : MonoBehaviour
 {
     static public FollowCam S; // a FollowCam Singleton
                                // fields set in the Unity Inspector pane
+    public float easing = 0.05f;
+    public Vector2 minXY;
     public bool _____________________________;
     // fields set dynamically
     public GameObject poi; // The point of interest
@@ -13,12 +15,17 @@ public class FollowCam : MonoBehaviour
         S = this;
         camZ = this.transform.position.z;
     }
-    void Update()
+    void FixedUpdate()
     {
         // if there's only one line following an if, it doesn't need braces
         if (poi == null) return; // return if there is no poi
                                  // Get the position of the poi
         Vector3 destination = poi.transform.position;
+        // Limit the X & Y to minimum values
+        destination.x = Mathf.Max(minXY.x, destination.x);
+        destination.y = Mathf.Max(minXY.y, destination.y);
+        // Interpolate from the current Camera position toward destination
+        destination = Vector3.Lerp(transform.position, destination, easing);
         // Retain a destination.z of camZ
         destination.z = camZ;
         // Set the camera to the destination
